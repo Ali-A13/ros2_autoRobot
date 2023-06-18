0e30d49de4c6c18a12c3d7cad1d23e49cf00899f
Md Khalique <md.j.khalique@gmail.com>
2023-06-18 18:54:27 +0000

    MD Khalique - My Commit (Colcon Build Test)

--- build/my_robot_package/prefix_override/sitecustomize.py
+++ build/my_robot_package/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/ac/ros2_auto_robot/install/my_robot_package'
+sys.prefix = sys.exec_prefix = '/home/ubuntu/Robot_Stuff/ros2_autoRobot/install/my_robot_package'
